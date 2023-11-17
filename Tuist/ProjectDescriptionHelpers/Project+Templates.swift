71a08a9cceb7782b1565d2e4cd68d5b928ee3672
박기석 <gkfnrkfl@naver.com>
2023-11-17 10:34:02 +0900

    feat: 타겟 생성 함수 추가

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -73,4 +73,42 @@ extension Project {
         ])
         return [mainTarget, testTarget]
     }
+    
+    /// bundle 기본 이름
+    private static var bundleName: String { "kr.kiseok" }
+    
+    /**
+     타겟 생성 함수
+     
+     - Parameters:
+        - name: 타겟 이름 설정.
+        - platform: 타겟 플랫폼 설정. default iOS
+        - product: 타겟 product 설정. (ex: app, unitTest, framework 등...)
+        - infoPlist: info.plist 설정
+        - sources: 소스 경로 설정
+        - resources: 리소스 경로 설정
+        - scripts: 스크립트 설정
+        - dependencies: 의존성 설정
+        - settings: 세팅 설정
+     */
+    public static func createTarget(name: String,
+                                    platform: Platform = .iOS,
+                                    product: Product,
+                                    infoPlist: InfoPlist = .default,
+                                    sources: SourceFilesList,
+                                    resources: ResourceFileElements? = nil,
+                                    scripts: [TargetScript] = [],
+                                    dependencies: [TargetDependency] = [],
+                                    settings: Settings? = nil) -> Target {
+        Target(name: name, // 타겟 이름 설정
+               platform: platform, // 타겟 플랫폼 설정
+               product: product, // 타겟 product 설정 (ex: app, unitTest, framework 등...)
+               bundleId: "\(bundleName).\(name)",
+               infoPlist: infoPlist, // info.plist 설정
+               sources: sources, // 소스 경로 설정
+               resources: resources, // 리소스 경로 설정
+               scripts: scripts, // 스크립트 설정
+               dependencies: dependencies, // 의존성 설정
+               settings: settings) // 세팅 설정
+    }
 }
