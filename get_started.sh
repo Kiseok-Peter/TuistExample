9ade53073b8b51a25349a0abbebb50296c97b662
박기석 <kiseok.park@thingsflow.com>
2023-11-23 09:38:43 +0900

    feat: 서브 모듈 추가 및 업데이트 스크립트에 추가

--- get_started.sh
+++ get_started.sh
@@ -1,1 +1,10 @@
+# git submodule add
+git submodule add git@github.com:Kiseok-Peter/CommonFoundation.git ./Modules/Common/CommonFoundation
+
+# git submodule update
+git submodule sync
+git submodule update --recursive --merge --remote
+
+
+# Tuist Generate
 tuist generate
