549b78dc278483a79340e9c14e3bfde627d63d34
박기석 <gkfnrkfl@naver.com>
2023-11-28 12:07:04 +0900

    feat: get_started에 tuist 설치 기능 추가

--- get_started.sh
+++ get_started.sh
@@ -1,3 +1,19 @@
+#tuist install
+function install_tuist_if_needed() {
+    echo ">> check tuist install\n"
+    RESULT=$(tuist local)
+    if [[ $RESULT != *"The following versions are available in the local environment"* ]]; then
+        echo "tuist install...\n"
+        curl -Ls https://install.tuist.io | bash
+        echo "tuist install success\n"
+    else
+        echo "tuist update...\n"
+        tuist update
+        echo "tuist update success\n"
+    fi
+}
+install_tuist_if_needed
+
 # git submodule add
 git submodule add git@github.com:Kiseok-Peter/CommonFoundation.git ./Modules/Common/CommonFoundation
 
