c7fd41601b342ae13f16a88e6a1d8108653c1056
박기석 <gkfnrkfl@naver.com>
2023-12-14 10:40:09 +0900

    fix: 기존 템플릿 제거

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,4 +1,5 @@
 import ProjectDescription
+import TuistTemplate
 
 /// Project helpers are functions that simplify the way you define your project.
 /// Share code to create targets, settings, dependencies,
@@ -6,44 +7,7 @@ import ProjectDescription
 /// See https://docs.tuist.io/guides/helpers/
 
 extension Project {
-    /// bundle 기본 이름
-    private static var bundleName: String { "kr.kiseok" }
-    
-    /**
-     타겟 생성 함수
-     
-     - Parameters:
-        - name: 타겟 이름 설정.
-        - platform: 타겟 플랫폼 설정. default iOS
-        - product: 타겟 product 설정. (ex: app, unitTest, framework 등...)
-        - deploymentTargetVersion: 타겟 deployment 버전. default 15.0
-        - infoPlist: info.plist 설정
-        - sources: 소스 경로 설정
-        - resources: 리소스 경로 설정
-        - scripts: 스크립트 설정
-        - dependencies: 의존성 설정
-        - settings: 세팅 설정
-     */
-    public static func createTarget(name: String,
-                                    platform: Platform = .iOS,
-                                    product: Product,
-                                    deploymentTargetVersion: String = "15.0",
-                                    infoPlist: InfoPlist = .default,
-                                    sources: SourceFilesList,
-                                    resources: ResourceFileElements? = nil,
-                                    scripts: [TargetScript] = [],
-                                    dependencies: [TargetDependency] = [],
-                                    settings: Settings? = nil) -> Target {
-        Target(name: name, // 타겟 이름 설정
-               platform: platform, // 타겟 플랫폼 설정
-               product: product, // 타겟 product 설정 (ex: app, unitTest, framework 등...)
-               bundleId: "\(bundleName).\(name)",
-               deploymentTarget: .iOS(targetVersion: deploymentTargetVersion, devices: [.iphone, .ipad]),
-               infoPlist: infoPlist, // info.plist 설정
-               sources: sources, // 소스 경로 설정
-               resources: resources, // 리소스 경로 설정
-               scripts: scripts, // 스크립트 설정
-               dependencies: dependencies, // 의존성 설정
-               settings: settings) // 세팅 설정
+    static func createProject() {
+//        Project(name: <#T##String#>)
     }
 }
