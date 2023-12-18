e92a123d2dc0ca36d35aa81c749413e710099731
박기석 <gkfnrkfl@naver.com>
2023-12-18 14:22:30 +0900

    fix: 플러그인 해시 수정

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let config = Config(
     plugins: [
         .local(path: .relativeToRoot("Plugins/TuistExample")),
-        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "e4422cd", directory: "Templates"),
-        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "e4422cd", directory: "StringSynthesizer")
+        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "7166681", directory: "Templates"),
+        .git(url: "git@github.com:Kiseok-Peter/TuistTemplate.git", sha: "7166681", directory: "StringSynthesizer")
     ]
 )
