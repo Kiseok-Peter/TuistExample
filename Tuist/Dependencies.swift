ee730a216e3d183d906b6ddcf2909fd9b452c786
박기석 <gkfnrkfl@naver.com>
2023-12-18 14:24:29 +0900

    feat: Dependencies.swift 추가

---
+++ Tuist/Dependencies.swift
@@ -0,0 +1,11 @@
+//
+//  Dependencies.swift
+//  Config
+//
+//  Created by 박기석 on 12/18/23.
+//
+
+import ProjectDescription
+
+let dependencies = Dependencies(swiftPackageManager: SwiftPackageManagerDependencies(),
+                                platforms: [.iOS])
